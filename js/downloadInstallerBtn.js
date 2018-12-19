c9bd704b4b480b3100de30e3bf3a6745e23d4400
Mauro Bieg <mb21@users.noreply.github.com>
2018-12-19 09:59:30 +0100

    Add downloadInstallerBtn.js

---
+++ js/downloadInstallerBtn.js
@@ -0,0 +1,48 @@
+(function() {
+  // from https://stackoverflow.com/a/38241481/214446
+  var userAgent = window.navigator.userAgent
+    , platform = window.navigator.platform
+    , macosPlatforms = ['Macintosh', 'MacIntel']
+    , windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
+    , iosPlatforms = ['iPhone', 'iPad', 'iPod']
+    , target = null
+    , detected
+    ;
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    detected = 'macOS';
+    target = '-macOS.pkg';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    if (userAgent.indexOf("WOW64") !== -1 || userAgent.indexOf("Win64") !== -1){
+      // from https://stackoverflow.com/a/6866569/214446
+      detected = 'Windows (64-bit)';
+      target = '-windows-x86_64.msi';
+    } else {
+      detected = 'Windows (32-bit)';
+      target = '-windows-i386.msi';
+    }
+  } else if (/Android/.test(userAgent)) {
+  } else if (!os && /Linux/.test(platform)) {
+    detected = 'Linux (64-bit)';
+    target = '-amd64.deb';
+  }
+
+  if (target) {
+    fetch("https://api.github.com/repos/jgm/pandoc/releases")
+    .then(function(data) {
+        return data.json();
+    })
+    .then(function(json) {
+      var assets = json[0].assets.filter(function(a){
+        return a.name.endsWith(target)
+      });
+      if (assets[0]) {
+        var btn = document.getElementById('downloadInstallerBtn');
+        btn.setAttribute('href', assets[0].browser_download_url);
+        var div = document.createElement('div');
+        div.innerHTML = 'Detected: ' + detected;
+        btn.parentNode.appendChild(div);
+      }
+    });
+  }
+})();
