3c57d535ea7e7e15309d95f631ad76199c332925
John MacFarlane <jgm@berkeley.edu>
2019-02-06 15:00:40 -0800

    Try again to make contents start uncollapsed.

--- js/collapseTOC.js
+++ js/collapseTOC.js
@@ -2,6 +2,6 @@
   var toc = document.getElementById('toc');
   var tocul = toc.getElementsByTagName('ul')[0];
   tocul.setAttribute('id', 'tocul');
-  // tocul.setAttribute('class', 'collapse');
+  tocul.setAttribute('class', 'collapse in');
   toc.innerHTML = '<div id="tocbtn"><a id="tocbtnlink" data-toggle="collapse" href="#tocul" role="button">[contents]</a></div>' + toc.innerHTML;
 })();
