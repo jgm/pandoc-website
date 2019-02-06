c95ba9d1e663b947625f4c2e2c3390a1698dfc38
John MacFarlane <jgm@berkeley.edu>
2019-02-06 15:04:55 -0800

    More js fix.

--- js/collapseTOC.js
+++ js/collapseTOC.js
@@ -4,4 +4,5 @@
   tocul.setAttribute('id', 'tocul');
   tocul.setAttribute('class', 'collapse in');
   toc.innerHTML = '<div id="tocbtn"><a id="tocbtnlink" data-toggle="collapse" href="#tocul" role="button">[contents]</a></div>' + toc.innerHTML;
+  tocul.setAttribute('aria-expanded', 'true');
 })();
