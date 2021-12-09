16528337ef23d385679c1c28bb56d46303c044f1
John MacFarlane <jgm@berkeley.edu>
2021-12-09 12:31:32 -0800

    Search: link to sections.

--- tools/build-index.js
+++ tools/build-index.js
@@ -14,9 +14,20 @@ function getHtmlText(fp) {
   try {
     var raw = fs.readFileSync(fp, 'utf8');
     const dom = new JSDOM(raw);
-    const text = dom.window.document.querySelector("main").textContent;
-    const title = dom.window.document.querySelector("title").textContent;
-    return { name: fp.replace(/^site\//,''), title: title, text: text };
+    const title = dom.window.document.querySelector("title").textContent.replace(/^Pandoc - /,'');
+    const matches = dom.window.document.querySelectorAll("section");
+    const basename = fp.replace(/^site\//,'');
+    var results = [];
+    matches.forEach(function(el) {
+      const sectident = el.getAttribute("id");
+      const secttitle = el.querySelector("h1") || el.querySelector("h2") ||
+                        el.querySelector("h3") || el.querySelector("h4");
+      results.push({ name: basename + "#" + sectident,
+                     title: title,
+                     sectionTitle: secttitle ? secttitle.textContent : null,
+                     text: el.textContent });
+    });
+    return results;
   } catch (err) {
     console.error(err);
     process.exit(1);
@@ -37,9 +48,18 @@ var titles = {};
 
 var files = glob.sync("site/*.html");
 files.forEach(function(file) {
+  stderr.write("Processing " + file + "\n");
   var doc = getHtmlText(file);
-  documents.push(doc);
-  titles[doc.name] = doc.title;
+  documents = documents.concat(doc);
+  doc.forEach(function(d) {
+    if (d.sectionTitle) {
+      stderr.write("  " + d.sectionTitle + "\n");
+      titles[d.name] = d.title + " - " + d.sectionTitle ;
+    } else {
+      stderr.write(  "(top)\n");
+      titles[d.name] = d.title ;
+    }
+  });
 });
 
 var idx = lunr(function () {
