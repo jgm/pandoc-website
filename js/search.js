60407270c25c3f2e4702c9439a42ab67a63589fb
John MacFarlane <jgm@berkeley.edu>
2021-12-09 12:39:12 -0800

    Search: do a conjunctive search by default.

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
 $("#form-search").submit(function(event) {
-  var results = idx.search($("#input-search").first().val());
+  var searchtext = $("#input-search").first().val().replace(/\b(\w+)/g,'+$1');
+  var results = idx.search(searchtext);
   $("#search-data").html("<p>" + results.length + " matches.</p>");
   for (var i = 0; i < results.length; i++) {
     var result = results[i];
