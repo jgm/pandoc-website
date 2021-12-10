3090a0d5531235fb7142de4381dce68ffee59d20
John MacFarlane <jgm@berkeley.edu>
2021-12-09 16:02:08 -0800

    Always list releases last in search.

--- js/search.js
+++ js/search.js
@@ -1,6 +1,14 @@
 $("#form-search").submit(function(event) {
   var searchtext = $("#input-search").first().val().replace(/\b(\w+)/g,'+$1');
   var results = idx.search(searchtext);
+  // put 'releases' matches at end
+  results.sort(function(first, second) {
+    if (first.ref.match(/^releases\.html/)) {
+      return 1 ;
+    } else {
+      return second.score - first.score ;
+    }
+  });
   $("#search-data").html("<p>" + results.length + " matches.</p>");
   for (var i = 0; i < results.length; i++) {
     var result = results[i];
